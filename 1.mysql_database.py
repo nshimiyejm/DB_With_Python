8e657899a657bee4c57c628d0212ef731a77dfa8
old_mac <old_mac@Administrators-MacBook-Pro-2.local>
2022-07-08 20:07:18 -0400

    Update 1.mysql_database.py

--- 1.mysql_database.py
+++ 1.mysql_database.py
@@ -9,13 +9,35 @@ def connect(db_name):
         # return the connection to the DB 
         return mysql.connect(
             host='localhost',
-            user='python_user',
-            password='TestL3arn!#$!',
+            user='user',
+            password='test',
             database = db_name
         )
     except Error as e:
         print(e)
-            
+
+def add_new_project(cursor, project_title, project_description, task_descriptions):
+    """
+    Method used to insert a project to the project table
+    VALUES will take a tuple to keep the data secure
+    """
+    project_data = (project_title, project_description)
+    
+    cursor.execute('''INSERT INTO projects(title, description) 
+                      VALUES (%s, %s)''', project_data)
+    
+    # Get the last project_id that was inserted then use it when instering a task
+    project_id = cursor.lastrowid
+    
+    tasks_data = []
+    
+    for description in task_descriptions:
+        task_data = (project_id, description)
+        tasks_data.append(task_data)
+    
+    cursor.executemany('''INSERT INTO tasks(project_id, description)
+                   VALUES (%s, %s)''', tasks_data)
+                
 
 if __name__ == '__main__':
     db = connect('projects')
@@ -23,11 +45,22 @@ if __name__ == '__main__':
     # Get the DB cursor 
     cursor = db.cursor()
     
+    # Create task descriptions
+    tasks = ["Clean bathroom", "Clean kitchen", "Clean living Room"]
+    # Call the add method 
+    add_new_project(cursor, "Clean House", "Clean House by Room", tasks)
+    
+    db.commit()
+    
     # Get all the projects
     cursor.execute("SELECT * FROM projects")
     project_records = cursor.fetchall()
     print(project_records)
     
+    # Get all tasks
+    cursor.execute("SELECT * FROM tasks")
+    tasks_records = cursor.fetchall()
+    print(tasks_records)
     
     db.close()
     
