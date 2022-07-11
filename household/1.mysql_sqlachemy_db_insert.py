0f347263d88c4f43fd31c9559f8bc77be761ee5c
old_mac <old_mac@Administrators-MacBook-Pro-2.local>
2022-07-11 11:14:39 -0400

    Update 1.mysql_sqlachemy_db_insert.py

--- household/1.mysql_sqlachemy_db_insert.py
+++ household/1.mysql_sqlachemy_db_insert.py
@@ -38,3 +38,32 @@ class Task(Base):
         return "<Task(description='{0}'>".format(self.description)
         
 Base.metadata.create_all(engine)
+
+# When using models, use a session to query the database - this can be achieved by using the sessionmaker 
+session_maker = sessionmaker()
+session_maker.configure(bind=engine)
+session = session_maker()
+
+# The session will allow the program to create transactions with the Database using SQLAlchemy 
+# Insert a project into the projects table
+organize_closet_project = Project(title='Organize closet', description='Organize closet by color and style')
+
+# Insert the data 
+session.add(organize_closet_project)
+# Commit the project to prevent the task from having a null project_id 
+session.commit()
+
+tasks = [Task(project_id=organize_closet_project.project_id, description='Decide what clothes to donate'), 
+         Task(project_id=organize_closet_project.project_id, description='Organize winter clothes'), 
+         Task(project_id=organize_closet_project.project_id, description='Organize summer clothes')]
+
+session.bulk_save_objects(tasks)
+session.commit()
+
+
+# Query documents from from the database 
+our_project = session.query(Project).filter_by(title='Organize closet').first()
+print(our_project)
+
+our_tasks  = session.query(Task).all()
+print(our_tasks)
